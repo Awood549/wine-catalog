2fda860759a26d3da11daa10b2e23ad2a74c9e9f
Awood549 <Austin.wood1776@live.com>
2020-03-03 12:28:27 -0800

    template object added

---
+++ src/resources/tastingObj.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.export = {
+  tasting: {
+    day: 'Mon/Wed',
+    date: '2/21',
+    theme: 'Domestic Reds',
+    wines:[
+      {
+        wine: 'cooper & theif',
+        notes: 'Very very vanilla',
+        crewRating: 2,
+      }
+    ],
+    varietals: {
+      redBlend: true,
+      cabernet:false,
+    },
+    region: {
+      domestic:true,
+      italy: false
+    }
+  }
+}
