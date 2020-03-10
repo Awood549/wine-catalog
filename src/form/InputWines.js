fbd1d26d2d24090ca482d9d04b015d55a797f019
Awood549 <Austin.wood1776@live.com>
2020-03-10 12:58:54 -0700

    setting up form for inputs

--- src/form/InputWines.js
+++ src/form/InputWines.js
@@ -5,9 +5,17 @@ function AddWine(){
 
   //https://www.youtube.com/watch?v=sfp5K_5GHNg
 
-  
+  function dayForm(){
+    return <form>
+      <input type = 'radio' id = 'monday' name = 'day' value = 'monday' ></input>
+      <label for = 'monday'> Monday</label>
+      <input type= 'radio' id= 'wednesday' name= ' day'  value = 'wednesday'></input>
+      <label for= 'wednesday'> Wednesday</label>
+    </form>
+  }
   return(
     <>
+    {dayForm()}
         <form>
       <button>
         Add A Tasting
