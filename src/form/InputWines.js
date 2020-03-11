3e0f3f277521675b9c59e4d12381d77966a2b2bb
Awood549 <Austin.wood1776@live.com>
2020-03-10 20:40:03 -0700

    form hook needs to be properly brought in, dinner though

--- src/form/InputWines.js
+++ src/form/InputWines.js
@@ -7,30 +7,28 @@ function AddWine(){
   //https://www.youtube.com/watch?v=sfp5K_5GHNg
 
   function dayForm(){
-    return <form>
+    return <>
       <input type = 'radio' id = 'monday' name = 'day' value = 'monday' ></input>
       <label htmlFor = 'monday'> Monday</label>
       <input type= 'radio' id= 'wednesday' name= ' day'  value = 'wednesday'></input>
       <label htmlFor= 'wednesday'> Wednesday</label>
-    </form>
+      <br></br>
+      </>
   }
   function dateForm(){
 
-    return <form>
+    return <>
       <input type = 'date' id = 'date' name = 'date'></input>
       <button onSubmit = {useForm.handleSubmit}>
         Here
       </button>
-    </form>
+      </>
   }
   return(
     <>
+    <form onSubmit = {useForm.handleSubmit}>
     {dayForm()}
     {dateForm()}
-        <form>
-      <button>
-        Add A Tasting
-      </button>
     </form>
 
     </>
